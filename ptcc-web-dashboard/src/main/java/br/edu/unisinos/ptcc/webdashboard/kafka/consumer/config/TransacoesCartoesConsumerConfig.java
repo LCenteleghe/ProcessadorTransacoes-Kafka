382d2ce3856d355d53b052d7f14ec2df294db9c9
luis.centeleghe <luis.centeleghe@SL-002889.cwinet.local>
2017-11-20 15:36:20 -0200

    Corrige import.

--- ptcc-web-dashboard/src/main/java/br/edu/unisinos/ptcc/webdashboard/kafka/consumer/config/TransacoesCartoesConsumerConfig.java
+++ ptcc-web-dashboard/src/main/java/br/edu/unisinos/ptcc/webdashboard/kafka/consumer/config/TransacoesCartoesConsumerConfig.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
